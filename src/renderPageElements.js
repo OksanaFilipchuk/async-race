70f754199db1b5110a6f1d1fd7ea2d73cab35b0c
OksanaFilipchuk <filipchuk.oksana@gmail.com>
2022-12-31 01:32:39 +0200

    feat: add Race, Reset functionality

--- src/renderPageElements.js
+++ src/renderPageElements.js
@@ -9,8 +9,17 @@ function renderPageElements() {
   winnerButton.textContent = "To Winner";
   winnerButton.className = "to-winner";
   winnerButton.addEventListener("click", switchToWinner);
+  let winnerPopUp = document.createElement("div");
+  winnerPopUp.className = "winner-pop-up";
+  winnerPopUp.innerHTML = `
+    <div class = "winner-pop-up-content">
+      <img class = "winner-img"src ="./src/winner.png" alt="winner"/>
+      <p class = "winner-name"></p>
+      <p class = "winner-time">Time: </p>
+    </div>`;
   header.appendChild(garageButton);
   header.appendChild(winnerButton);
+  header.appendChild(winnerPopUp);
   let garageBlock = document.createElement("div");
   garageBlock.className = "garage-block";
   garageBlock.innerHTML = `
